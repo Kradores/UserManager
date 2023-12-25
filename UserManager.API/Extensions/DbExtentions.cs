50d766e11219dba6ad12ba25e14325bc79b1582b
krado <krado@KRADORES-PC>
2023-12-26 00:24:49 +0100

    Add project files.

---
+++ UserManager.API/Extensions/DbExtentions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManager.Infrastructure;
+
+namespace UserManager.API.Extensions;
+
+internal static class DbExtentions
+{
+    public static IServiceCollection AddDbService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<UserDbContext>(o => o.UseNpgsql(configuration["ConnectionStrings:UserManager"]));
+        return services;
+    }
+}
---
+++ UserManager.Infrastructure/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserManager.Infrastructure
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("users");
+        }
+    }
+}
